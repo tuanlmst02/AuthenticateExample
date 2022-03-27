1c7a5af57f620e9faf9eade5e28ecd085279343d
tuanlm <tuanlmst02@gmail.com>
2022-03-27 22:24:11 +0700

    220327_CommitSource

---
+++ AuthenticationExample/AuthenticationExample/Controllers/AuthController.cs
@@ -0,0 +1,43 @@
+﻿using AuthenticationExample.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace AuthenticationExample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        [HttpPost, Route("login")]
+        public IActionResult Login([FromBody] LoginModel user)
+        {
+            if (user == null)
+            {
+                return BadRequest("Invalid account request");
+            }
+
+            if (user.UserName == "tuanlm" && user.Password == "@123")
+            {
+                var secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("privateSecretKey@123"));
+                var signinCredentials = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256);
+
+                var tokeOptions = new JwtSecurityToken(
+                    audience: "Minh Tuan Le",
+                    claims: new List<Claim>(),
+                    expires: DateTime.Now.AddMinutes(15),
+                    signingCredentials: signinCredentials
+                );
+
+                var tokenString = new JwtSecurityTokenHandler().WriteToken(tokeOptions);
+                return Ok(new { Token = tokenString });
+            }
+            else
+            {
+                return Unauthorized();
+            }
+        }
+    }
+}
